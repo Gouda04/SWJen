d7d64e171030805de21e0d073bbb93e869df6b59
Gouda04 <meno.1234.hossam@gmail.com>
2024-12-25 01:57:36 +0200

    version1

---
+++ Models/BrandContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SWPROJECT.Models
+{
+    public class BrandContext : DbContext
+    {
+        public BrandContext(DbContextOptions<BrandContext> options) : base(options)
+        {
+        }
+        public DbSet<Brand> Brands { get; set; }
+
+    }
+}
